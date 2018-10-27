81b1e1f8a6abc49dc4f143d94b08adf6163f39ed
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-27 19:57:15 -0400

    Created Project

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PrivateHeader from './PrivateHeader'
+export default () => (
+    <div>
+        <PrivateHeader title="Your Library"/>
+        <div className="page-content">
+            <h1>Your Library</h1>
+        </div>
+    </div>
+)
